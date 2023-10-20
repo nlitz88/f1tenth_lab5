81831bc2505bd964e38214438ce60ebd016a7d0c
Nathan Litzinger <nlitz88@gmail.com>
2023-10-20 02:20:16 -0400

    Fixed path publisher, added parameter file and launch directory. Updated CMAKE lists to install path files as well

--- pure_pursuit/scripts/path_publisher.py
+++ pure_pursuit/scripts/path_publisher.py
@@ -1,36 +1,52 @@
 #!/usr/bin/env python3
 
+from typing import Optional
 import rclpy
 from rclpy.node import Node
 from rclpy.duration import Duration
 from nav_msgs.msg import Path
-from geometry_msgs.msg import Point, Pose
+from geometry_msgs.msg import Point, Pose, PoseStamped
 
 import csv
 
 class PathFilePublisher2D(Node):
 
-    def __init__(self, filepath: str) -> None:
+    def __init__(self, filepath: Optional[str] = None) -> None:
         super().__init__(node_name="path_file_publisher")
+        
+        # Initialize Parameters.
+        # NOTE: This call to declare parameters will take any parameter values
+        # provided alongside executing this node. I.e., in a launch file, in a
+        # params file, as cli args in the ros run command, etc.
+        self.declare_parameters(
+            namespace="",
+            parameters=[
+                ("filepath", filepath)
+            ],
+            ignore_override=False
+        )
+        self.__filepath = self.get_parameter("filepath").value
 
         # Before anything else: attempt to load the path from the file at the
         # specified filepath. If able to load the file, create a "Path" object
         # from the sequence of points.
         try:
-            with open(filepath, 'r') as path_file:
+            with open(self.__filepath, 'r') as path_file:
                 csv_reader = csv.reader(path_file)
                 self.__path = Path()
                 for row in csv_reader:
-                    point = Point(x=row[0], y=row[1])
-                    pose = Pose(position=point)
-                    self.__path.poses.append(pose)
+                    if len(row) == 2:
+                        point = Point(x=float(row[0]), y=float(row[1]))
+                        pose = Pose(position=point)
+                        pose_stamped = PoseStamped(pose=pose)
+                        self.__path.poses.append(pose_stamped)
         except OSError as exc:
-            self.get_logger().error(f"Failed to open path file {filepath}")
+            self.get_logger().error(f"Failed to open path file {self.__filepath}")
             raise exc
-        self.get_logger().info(f"Successfully created new Path message from {len(self.__path.poses)} waypoints found in path file {filepath}")
+        self.get_logger().info(f"Successfully created new Path message from {len(self.__path.poses)} waypoints found in path file {self.__filepath}")
         
         # Create a timer that will generate interrupts to trigger the publisher.
-        self.create_timer(timer_period_sec=Duration(seconds=0.1), 
+        self.create_timer(timer_period_sec=0.1, 
                           callback=self.__publish_path)
 
         # Create publisher. Will publish at a fixed interval. Additionally, will
@@ -50,9 +66,9 @@ class PathFilePublisher2D(Node):
         """
         self.__path_publisher.publish(self.__path)
 
-def main(*args, **kwargs):
+def main(args=None):
     rclpy.init(args=args)
-    path_publisher = PathFilePublisher2D(filepath="rats")
+    path_publisher = PathFilePublisher2D()
     rclpy.spin(path_publisher)
     path_publisher.destroy_node()
     rclpy.shutdown()
