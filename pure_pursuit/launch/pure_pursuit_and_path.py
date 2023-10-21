38a611a933cbcbefe29b5d9b1bae7c1b40c74db2
Nathan Litzinger <nlitz88@gmail.com>
2023-10-21 00:54:19 -0400

    Added launch file for path file publisher and pure pursuit node

---
+++ pure_pursuit/launch/pure_pursuit_and_path.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    
+    # Initialize a new launch description.
+    ld = LaunchDescription()
+    
+    # Get path to the params.yaml file.
+    params = Path(get_package_share_directory("pure_pursuit"), "config", "params.yaml")
+
+    # Create new actions to spin up nodes for the pure pursuit node and path
+    # file publisher node.
+    # pure_pursuit_node = Node(
+    #     executable="pure_pursuit"
+    # )
+    path_file_publisher_node = Node(
+        package="pure_pursuit",
+        executable="path_publisher.py",
+        parameters=[params]
+    )
+    # Add the launch_ros "Node" actions we created.
+    # ld.add_action(pure_pursuit_node)
+    ld.add_action(path_file_publisher_node)
+
+    # Return the newly created launch description.
+    return ld
--- pure_pursuit/pure_pursuit/pure_pursuit.py
+++ pure_pursuit/pure_pursuit/pure_pursuit.py
@@ -4,6 +4,8 @@ from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from nav_msgs.msg import Path
+from geometry_msgs.msg import Pose, PoseStamped
 # TODO CHECK: include needed ROS msg type headers and libraries
 
 class PurePursuit(Node):
@@ -12,13 +14,52 @@ class PurePursuit(Node):
     This is just a template, you are free to implement your own node!
     """
     def __init__(self):
-        super().__init__('pure_pursuit_node')
+        super().__init__("pure_pursuit")
+        
+        # Set up node parameters.
+        self.declare_parameters(namespace="",
+                                parameters=[
+                                    ("lookahead_distance_m", rclpy.Parameter.Type.DOUBLE),
+                                    ("max_longitudinal_velocity_ms", rclpy.Parameter.Type.DOUBLE),
+                                    ("min_longitudinal_velocity_ms", rclpy.Parameter.Type.DOUBLE)
+                                ])
+
         # TODO: create ROS subscribers and publishers
 
     # Fundamental Question: What format are points typically provided in? I
-    # actually made this an action item in 
+    # actually made this an action item in my notes.
+    # Well, when in the other module I have, I just have a function that loads
+    # them in and turns them into a proper Path message from the nav library.
+    # Could use that here, as that probably is the best, most feature reach
+    # collection of values.
 
-    def get_next_target_point(self, pose, path_waypoints):
+
+    def get_next_target_point(self, current_pose: PoseStamped, path: Path) -> Pose:
+        """Function that will take the robot's current pose in the map frame and
+        determine what the next target point should be.
+
+        Specifically, this implementation looks at which point in the path array
+        is closest the lookahead distance away from the vehicle.
+
+        Args:
+            current_pose (PoseStamped): The robot's current pose in the map
+            frame / w.r.t. the map frame.
+            path (Path): A sequence of robot poses, each with respect to the map
+            frame as well.
+
+        Returns:
+            Pose: The point in the path chosen as the next target point.
+        """
+        # Create a vectorized version of the (x,y) positions in the path using
+        # numpy.
+
+        # Apply a euclidean distance elementwise across the vector of (x,y) path
+        # positions.
+
+        # Find the point that is closest to the current pose. We'll start our
+        # search for the point closest to the lookahead distance from this
+        # point.
+        
         pass
 
     def transform_target_to_vehicle_frame(self, target_point):
